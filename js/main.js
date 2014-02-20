67cbe28d99c1170d565dd27917f57cbba8230e95
Jake Ruesink <jaruesink@gmail.com>
2014-02-19 18:00:51 -0600

    added navbar on scroll and history.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-$(function(){
+//Define Functions
+    //change the window hash
+    var changeHash = function(a){ document.location.hash = a };
+    //get the scrollPos
+    var scrollPos = function(){ $(window).scrollTop() };
+    //find the position of the top of element
+    var topPos = function(a){ var thistop = $(a).offset().top; return thistop; };
+    //find the position of the bottom of element
+    var bottomPos = function(a){ var thisbottom = $(a).offset().top + $(a).height(); return thisbottom; };
 
-};
+
+//FadeIn the MenuBar and change hash names on scroll
+    $(window).scroll(function() {    
+        if ($(this).scrollTop() < bottomPos('#top')) {$('.navbar').fadeOut('fast');}
+        else {$('.navbar').fadeIn('fast');}
+    });
+
+//SmoothScroll
+    $('a[href*=#]:not([href=#])').click(function() {
+    var hashName = this.hash;
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(hashName);
+      History.pushState();
+      if (target.length) {
+        $('html,body').animate({scrollTop: target.offset().top}, 1000, function(){changeHash(hashName)});
+        return false;
+      }
+    }
+    });
