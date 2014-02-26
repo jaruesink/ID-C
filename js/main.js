f037f420690cb08932aaf67cdeface560590da67
Jake Ruesink <jake.ruesink@gsdm.com>
2014-02-26 17:11:03 -0600

    added members JSON
    also some functionality for clicking on profiles - still a little jumpy

--- js/main.js
+++ js/main.js
@@ -8,6 +8,23 @@
     //find the position of the bottom of element
     var bottomPos = function(a){ var thisbottom = $(a).offset().top + $(a).height(); return thisbottom; };
 
+//JSON for Member List
+for (var i=0; i<10; i++){
+    $('.diamond#member'+i+'').click(function(){
+        var thisid = this.id;
+        var x = thisid.substring(6,10);
+        $.getJSON("js/members.json", function(data){
+            var member = data.members[x];
+            document.getElementById('membername').innerHTML = member.name;
+            document.getElementById('memberposition').innerHTML = member.position;
+            document.getElementById('memberdescription').innerHTML = member.description;
+            $('#memberinfo').slideDown();
+        })
+    });
+}
+$('#memberinfo .close').click(function(){
+    $('#memberinfo').slideUp();
+});
 
 //FadeIn the MenuBar and change hash names on scroll
     $(window).scroll(function() {    
